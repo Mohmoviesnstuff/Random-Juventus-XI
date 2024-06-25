0fd1d2943b4cf9a27561f1b4a3f508c711af8d68
Mohmoviesnstuff <sanusimohammed0000@gmail.com>
2024-06-25 10:53:17 -0500

    Add files via upload

---
+++ juventus.py
@@ -0,0 +1,16 @@
+import random
+#printing my intro statement
+print("WELCOME TO THE RANDOM JUVE XI GENERATOR")
+#creating the loop
+while True:
+    for i in range(1):
+        entrance = input("Press enter to continue")
+        Goalie = ["Sczesny", "Buffon", "Perin", "Pingosolio", "Tacconi"] 
+        Lb = ["Sandro", "Evra", "Kostic", "Zambrotta", "Montero"]
+        print("GK- ", random.choices(Goalie)) 
+        print("LB- ", random.choices(Lb) )
+        print("©mohtech")
+    continue
+    
+        
+
